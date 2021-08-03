c2a3f03a28655e97ece386351f2a4acc35a8b30b
avoysarkar <73129752+avoysarkar@users.noreply.github.com>
2021-08-03 23:19:35 +0530

    Use of React Props

---
+++ src/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.tel}</p>
+      <p>{props.web}</p>
+    </div>
+  );
+}
+
+export default Card;
